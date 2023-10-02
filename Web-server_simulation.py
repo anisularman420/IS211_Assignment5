3f62dd611509471d9470a2842a59be60b57aa2f8
anisularman420 <Anisul.Arman19@spsmail.cuny.edu>
2023-10-02 18:15:03 -0400

    The results indicate the average wait time for requests in both scenarios.

--- Web-server_simulation.py
+++ Web-server_simulation.py
@@ -1,11 +1,13 @@
-import requests
 import csv
+import requests
+
 
 class Request:
     def __init__(self, timestamp, url, processing_time):
         self.timestamp = timestamp
         self.url = url
         self.processing_time = processing_time
+
 
 class Server:
     def __init__(self):
@@ -25,9 +27,22 @@ class Server:
             if self.time_remaining <= 0:
                 self.current_request = None
 
+
+class LoadBalancer:
+    def __init__(self, num_servers):
+        self.num_servers = num_servers
+        self.servers = [Server() for _ in range(num_servers)]
+        self.current_server = 0
+
+    def get_next_server(self):
+        server = self.servers[self.current_server]
+        self.current_server = (self.current_server + 1) % self.num_servers
+        return server
+
+
 def simulate_one_server(input_url):
     response = requests.get(input_url)
-    
+
     if response.status_code != 200:
         print("Failed to download the CSV file.")
         return
@@ -57,7 +72,49 @@ def simulate_one_server(input_url):
     average_wait_time = total_wait_time / len(request_list)
     return average_wait_time
 
+
+def simulate_many_servers(input_url, num_servers):
+    response = requests.get(input_url)
+
+    if response.status_code != 200:
+        print("Failed to download the CSV file.")
+        return
+
+    request_list = []
+
+    csv_content = response.text.splitlines()
+    csv_reader = csv.reader(csv_content)
+    next(csv_reader)  # Skip the header row
+
+    for row in csv_reader:
+        timestamp, url, processing_time = row
+        request = Request(int(timestamp), url, int(processing_time))
+        request_list.append(request)
+
+    load_balancer = LoadBalancer(num_servers)
+    total_wait_time = 0
+
+    for request in request_list:
+        server = load_balancer.get_next_server()
+        if server.is_busy():
+            total_wait_time += server.time_remaining
+            server.tick()
+        server.start_next(request)
+        total_wait_time += server.time_remaining
+        server.tick()
+
+    average_wait_time = total_wait_time / len(request_list)
+    return average_wait_time
+
+
 if __name__ == "__main__":
     input_url = "http://s3.amazonaws.com/cuny-is211-spring2015/requests.csv"
+
+    # Part I: Simulate with one server
     average_latency_single_server = simulate_one_server(input_url)
     print(f"Average latency for single server: {average_latency_single_server:.2f} seconds")
+
+    # Part II: Simulate with multiple servers (load balancer)
+    num_servers = 2  # Replace with the number of servers you want to simulate
+    average_latency_many_servers = simulate_many_servers(input_url, num_servers)
+    print(f"Average latency for {num_servers} servers: {average_latency_many_servers:.2f} seconds")
